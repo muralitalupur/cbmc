7154aec803bc58d9ccff2905031af0c8f8879fd9
Lefan Zhang <lefanz@amazon.com>
2020-09-19 04:23:50 +0000

    file_util: another attempt

--- src/util/file_util.cpp
+++ src/util/file_util.cpp
@@ -96,15 +96,8 @@ std::string get_absolute_path(const std::string &rel_path)
   free(wd);
 #else
   TCHAR buffer[4096];
-  TCHAR rel_path_t[4096];
-  std::wstring rel_path_w = utf8_to_utf16_native_endian(rel_path);
-#  ifdef UNICODE
-  wcscpy(rel_path_t, rel_path_w.c_str());
-#  else
-  strcpy(rel_path_t, narrow(rel_path_w).c_str());
-#  endif
   DWORD retval = GetFullPathName(
-    rel_path_t, 4096, buffer, "");
+    widen(rel_path).c_str(), 4096, buffer, NULL);
   if(retval == 0)
     throw system_exceptiont("failed to get current directory of process");
 
